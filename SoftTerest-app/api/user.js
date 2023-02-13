eb0b0df1c6b738f409a27744882751e148c749df
Valentin <valio990210@gmail.com>
2023-02-13 17:22:45 +0200

    logout method

--- SoftTerest-app/api/user.js
+++ SoftTerest-app/api/user.js
@@ -8,12 +8,16 @@ const endPoint = {
 
 export async function login(email, password){
     const user = await api.post(endPoint.login, {email, password})    //sets the method POST in api.js and takes the data 
-    localStorage.setItem('user', JSON.stringify(user));             //stores the user in the localstorage
+    sessionStorage.setItem('user', JSON.stringify(user));             //stores the user in the localstorage
 }
 
 
 export async function register(email, password){
     const user = await api.post(endPoint.register, {email, password})    //sets the method POST in api.js and takes the data 
+    sessionStorage.setItem('user', JSON.stringify(user))
 
+}
 
+export async function logout(){
+    api.get(endPoint.logout)
 }
