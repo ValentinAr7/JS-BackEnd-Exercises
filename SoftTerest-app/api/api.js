af5ba45faa1d6de80ed7cbffe5d75f6b966558df
Valentin <valio990210@gmail.com>
2023-02-13 17:31:36 +0200

    error handling

--- SoftTerest-app/api/api.js
+++ SoftTerest-app/api/api.js
@@ -1,30 +1,45 @@
- const host = 'http://localhost:3030';
+const host = 'http://localhost:3030';
 
- async function requester (method, url, data){      //create a requester
+async function requester(method, url, data) {      //create a requester
     const option = {                                //check the method if GET fetch
         method,                                     // if POST add header and body
         headers: {}
     }
 
-    if(data){
+    if (data) {
         option.headers['Content-Type' = 'Application/json'];    //add header
         option.body = JSON.stringify(data);                     //add body
     }
 
-    const response = await fetch (host + url, option)           //fetch data from the server
+
+    try {
+        const response = await fetch(host + url, option)           //fetch data from the server
+        if (!response.ok) {
+            const err = await response.json();
+            throw new Error(err.message);
+        }
+
+        if (response.status === 204) {
+            return response
+        } else {
+            return response.json()
+        }
+    } catch (error) {
+        alert(error)
+    }
     const data = await response.json()
 
-    return data
- }
+
+}
 
 const get = requester.bind(null, 'get');            //send the method to the requester
 const post = requester.bind(null, 'post');          //content === null
 const put = requester.bind(null, 'put');
 const del = requester.bind(null, 'delete');
 
- export {
+export {
     get,
     post,
-    put, 
+    put,
     del as delete
- }
+}
