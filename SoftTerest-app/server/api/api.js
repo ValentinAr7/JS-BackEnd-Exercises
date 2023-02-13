bba3866bb160bea6c76cf0d61c04d402c469eda0
Valentin <valio990210@gmail.com>
2023-02-13 16:45:24 +0200

    comments

--- SoftTerest-app/server/api/api.js
+++ SoftTerest-app/server/api/api.js
@@ -1,16 +1,16 @@
  const host = 'http://localhost:3030';
 
- async function requester (method, url, data){
-    const option = {
-        method,
+ async function requester (method, url, data){      //create a requester
+    const option = {                                //check the method if GET fetch
+        method,                                     // if POST add header and body
         headers: {}
     }
 
     if(data){
-        option.headers['Content-Type' = 'Application/json'];
-        option.body = JSON.stringify(body);
+        option.headers['Content-Type' = 'Application/json'];    //add header
+        option.body = JSON.stringify(body);                     //add body
     }
 
-    const response = await fetch (host + url, option)
+    const response = await fetch (host + url, option)           //fetch data from the server
 
  }
